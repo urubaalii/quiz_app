91c8a206d70b72fafa7f3f2ff8168b33c6e343b0
urubaalii <urubaali01@gmail.com>
2026-02-10 11:46:59 -0500

    Initial Screen (start)

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,48 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 63, 8, 165),
+            Color.fromARGB(255, 12, 1, 39),
+          ],
+
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/quiz-logo.png', width: 280, color: const Color.fromARGB(200, 255, 255, 255)),
+            const SizedBox(height: 80),
+            Text(
+              "Challenge yourself on this quiz!",
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+            const SizedBox(height:30), 
+            OutlinedButton.icon(
+              onPressed: () {}, 
+              icon:const Icon(Icons.arrow_circle_right_outlined),
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            label: Text("Start Quiz")
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
 
     return Container(
       decoration: BoxDecoration(
