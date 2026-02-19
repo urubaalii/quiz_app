99aba4833eadbca4976b46208bcdb2179dd09aee
urubaalii <urubaali01@gmail.com>
2026-02-19 12:03:14 -0500

    Did some basic styling on the answer buttons

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -22,10 +22,12 @@ class AnswerButton extends StatelessWidget{
           vertical: 10,
           horizontal: 40,
         ),
-        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+        backgroundColor: const Color.fromARGB(255, 98, 59, 175),
         foregroundColor: Colors.white,
       ),
-      child: Text(answerText),
+      child: Text(answerText,
+      textAlign: TextAlign.center,
+      ),
     );
   }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -18,21 +18,26 @@ extends State<QuestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(currentQuestion.question,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          )
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers.map((item){
-            return AnswerButton(answerText: item,
-            onTap:(){});
-          })
-        ],),
+      child: Container( //added container here to add margin to the content
+        margin:const EdgeInsets.all(20), //this puts space between edge of screen and the content
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch, //made buttons stretch across the screen
+          children: [
+            Text(currentQuestion.question,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+            textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers.map((item){ //function that creates a list of answer buttons from the list of answers 
+              return AnswerButton(answerText: item,
+              onTap:(){});
+            })
+          ],),
+      ),
     );
   }
 }
